f9932fd6704dedf97048e4d74d4e212e9ebac89c
Williamlam343 <87791763+Williamlam343@users.noreply.github.com>
2021-08-22 15:28:50 -0400

    css updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 const colors = require('tailwindcss/colors')
 
+
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
@@ -19,14 +21,31 @@ module.exports = {
         sky: colors.sky,
       },
     }, 
-
+    glow: {
+      colors: { // Defaults to all theme colors
+        blue: theme("colors.blue.100"),
+        // ...
+      },
+      styles: { // Defaults to these values
+        default: baseColor =>
+          `0 1px 3px 0 rgba(${baseColor}, 0.4), 0 1px 2px 0 rgba(${baseColor}, 0.24)`,
+        md: baseColor =>
+          `0 4px 6px -1px rgba(${baseColor}, 0.4), 0 2px 4px -1px rgba(${baseColor}, 0.24)`,
+        lg: baseColor =>
+          `0 10px 15px -3px rgba(${baseColor}, 0.4), 0 4px 6px -2px rgba(${baseColor}, 0.20)`,
+        xl: baseColor =>
+          `0 20px 25px -5px rgba(${baseColor}, 0.4), 0 10px 10px -5px rgba(${baseColor}, 0.16)`,
+        "2xl": baseColor => `0 25px 50px -12px rgba(${baseColor}, 1)`,
+        outline: baseColor => `0 0 0 3px rgba(${baseColor}, 0.5)`,
+        none: "none"
+      }
   },
   variants: {
     extend: {},
   },
   plugins: [ 
     
-    require("tailwindcss-glow")
+    require("tailwindcss-glow"),
   
   ],
-}
+  }}
